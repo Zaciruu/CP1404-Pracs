f307ee015f688f76592caa98fdf21cd6598102cd
Zac <zbramham@gmail.com>
2018-08-21 15:53:08 +1000

    Lists for Prac

--- Prac 4/lists_warmup.py
+++ Prac 4/lists_warmup.py
@@ -7,3 +7,11 @@ numbers[-1] = 1
 print(numbers)
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
+print(str(numbers[-1]), str(numbers[-2]))
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+if numbers.index(9):
+    print("true")
+else:
+    print("false")
+
