a9319b34eb7046142e47a33fd46fa508e4b0e1a7
Zac <zbramham@gmail.com>
2018-09-05 16:45:40 +1000

    Pep 8.

--- Prac_6/car.py
+++ Prac_6/car.py
@@ -14,9 +14,8 @@ class Car:
         self.odometer = 0
 
     def __str__(self):
-        """Return a string representation of a Car object."""
-        return "{}, fuel={}, odometer={}".format(self.name, self.fuel,
-                                                 self.odometer)
+        return "{}, Fuel = {}, Odometer = {}".format(self.name, self.fuel,
+                                                     self.odometer)
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
