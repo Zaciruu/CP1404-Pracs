9e4f803e9663dde981d9067eba5e1a7ba29caaf3
Zac <zbramham@gmail.com>
2018-09-05 16:45:18 +1000

    Refined code.

--- Prac_6/used_cars.py
+++ Prac_6/used_cars.py
@@ -6,14 +6,20 @@ from Prac_6.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    my_car.drive(30)
-    print("fuel =", my_car.fuel)
-    print("odo =", my_car.odometer)
-    print(my_car)
-
-    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
-    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+    # my_car = Car(180)
+    # my_car.drive(30)
+    # print("fuel =", my_car.fuel)
+    # print("odo =", my_car.odometer)
+    # print(my_car)
+    #
+    # print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    # print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    limo.drive(115)
+    print("Limo Fuel = {}".format(limo.fuel))
+    print("Limo Odo = {}".format(limo.odometer))
+    print(limo)
 
 
 main()
