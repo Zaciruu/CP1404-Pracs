95b57e2c1a4e4fbd29f657623fc2b98024d2010c
Zac <zbramham@gmail.com>
2018-09-05 15:50:19 +1000

    Upload

--- Prac 5/string_counter.py
+++ Prac 5/string_counter.py
@@ -1,10 +1,14 @@
-words_to_count = []
-text = input("Please enter some text")
-word_list = text.strip().split(",")
-for word in word_list:
-    if word in words_to_count:
-        words_to_count[word] += 1
-    else:
-        words_to_count[word] = 1
-print(text)
+unique_words = {}
+text = input("Words: ")
+words = text.split()
+for word in words:
+    frequency = unique_words.get(word, 0)
+    # Note: this is the "Look Before You Leap" (LBYL) pattern
+    # we could use the "Easier to Ask Forgiveness" pattern using exceptions
+    unique_words[word] = frequency + 1
 
+words = list(unique_words.keys())
+words.sort()
+max_length = max((len(word) for word in words))
+for word in words:
+    print("{:{}} : {}".format(word, max_length, unique_words[word]))
