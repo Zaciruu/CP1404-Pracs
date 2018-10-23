55254f87a1b00b891877dca40254b8050b80e8e7
Zac <zbramham@gmail.com>
2018-10-23 11:07:34 +1000

    Init commit

--- Prac_6/guitars.py
+++ Prac_6/guitars.py
@@ -5,18 +5,18 @@ def main():
     guitars = []
 
     print("My Guitars")
-    name = input("Name: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: $"))
-        add_guitar = Guitar(name, year, cost)
-        guitars.append(add_guitar)
-        print("{} added".format(add_guitar))
-        name = input("Name: ")
+    # name = input("Name: ")
+    # while name != "":
+    #     year = int(input("Year: "))
+    #     cost = float(input("Cost: $"))
+    #     add_guitar = Guitar(name, year, cost)
+    #     guitars.append(add_guitar)
+    #     print("{} added".format(add_guitar))
+    #     name = input("Name: ")
 
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-
+    print(guitars)
     print("These are my guitars:")
 
     if guitars is not None:
